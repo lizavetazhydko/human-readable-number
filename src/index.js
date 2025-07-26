fc0656926efed26c9ecd86ae8b28b6ad30851776
Lizaveta Zhydko <lizavetazhydko@gmail.com>
2025-07-26 18:23:59 +0200

    init: start human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(number) {
+  const ones = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+
+  const teens = [
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+
+  const tens = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+
+  if (number === 0) return 'zero';
+
+  let result = '';
+  let remainder = number;
+
+  if (remainder >= 100) {
+    result += `${ones[Math.floor(remainder / 100)]} hundred`;
+    remainder %= 100;
+    if (remainder > 0) result += ' ';
+  }
+
+  if (remainder >= 20) {
+    result += tens[Math.floor(remainder / 10)];
+    if (remainder % 10 > 0) {
+      result += ` ${ones[remainder % 10]}`;
+    }
+  } else if (remainder >= 10) {
+    result += teens[remainder - 10];
+  } else if (remainder > 0) {
+    result += ones[remainder];
+  }
+
+  return result;
 };
